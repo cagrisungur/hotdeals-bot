6e8a955fcd1c792c0572f4415af3e5e28d859659
Cagri Sungur <cagri@90pixel.com>
2021-05-01 16:36:31 +0300

    knex file js dynamically from .env file

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,12 @@
-// Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
     client: 'mysql',
     connection: {
-        host: '127.0.0.1',
-      user:'root',
-      password:'',
-      database:'hotdeals'
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE
     },
     migrations: {
         directory: './db/migrations'
